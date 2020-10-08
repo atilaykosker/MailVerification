efe7861c5e790210717f31ec6026ed6cbcb924ed
Atılay Köşker <41235268+atilaykosker@users.noreply.github.com>
2020-10-08 17:21:11 +0300

    Update files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RobocopID.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobocopID.Data
+{
+
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
